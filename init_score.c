a7bbe928e248b49da99591675ad4b3f126237aa0
GIUSEPE TESSARI L. DE OLIVEIRA <16104033@l1801323.portoalegre.pucrsnet.br>
2016-06-06 10:53:47 -0300

    #2

---
+++ init_score.c
@@ -0,0 +1,10 @@
+#include "init_score.h"
+//Inicializa a estrutura de pontuação do jogo(score)
+
+void init_score() {
+
+	score.shots = 0;
+	score.points = 0;
+	score.level = 1;
+}
+
---
+++ init_score.h
@@ -0,0 +1,13 @@
+
+//Inicializa a estrutura de pontuação do jogo(score)
+
+struct score_t {
+
+	unsigned int shots;
+	unsigned int points;
+	unsigned int level;
+};
+
+struct score_t score;
+
+void init_score();
